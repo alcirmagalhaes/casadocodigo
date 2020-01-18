f72ca0b647145e0e707f5b9fbc0b4064fc7ad775
alcir <alcirmagalhaes@gmail.com>
2020-01-18 09:08:29 -0300

    Node p1-02-3

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
+
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function (req, resp) {
     console.log('servidor rodando na porta 3000')
@@ -39,35 +39,3 @@ app.get('/livros', function (req, resp) {
     );
     
 })
-/* const http = require('http');
-const servidor = http.createServer(function (req, resp) {
-
-    let html = '';
-    if (req.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-        `;
-    } else if (req.url == '/livros') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Listagem de livros </h1>
-                </body> 
-            </html>
-        `;
-    }
-
-    resp.end(html);
-    
-});
-servidor.listen(3000); */
